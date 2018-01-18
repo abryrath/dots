cdd49645c9db5f5671441b8df989cdb256d2c158
Abry Rath <abryrath@gmail.com>
2018-01-17 19:26:29 -0500

    Deleting old files

--- .emacs.d/my-flycheck.el
+++
@@ -1,6 +0,0 @@
-					; for python, need `pip install pylint`
-					; for js, need `npm install eslint`
-					; for ruby, need `gem install rubocop ruby-lint reek
-					; for SQL, need `gem install sqlint`
-(require 'flycheck)
-(add-hook 'after-init-hook #'global-flycheck-mode)
