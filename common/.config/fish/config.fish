fac525a60eea14b7d0347d778026e6481da6993c
Abry Rath <abry.rath@union.co>
2020-09-01 09:41:03 -0400

    Latest changes

--- common/.config/fish/config.fish
+++ common/.config/fish/config.fish
@@ -1,42 +1,16 @@
-
-set fish_user_paths $HOME/.local/bin $PATH
-if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
-end
-
-if not type -q bass
-    echo "bass not installed..., to install:"
-    echo "  fisher add edc/bass"
-    #fisher add edc/bass
-    #echo "Finished"
-end
-
-bass source ~/.local/share/env.sh
-
-#if type -q bass
-#    bass source ~/.aliases
-#end
-for func in ~/.config/fish/user-functions/*
-    # 	echo "Loading $func"
-    source $func
-end
+set fish_greeting ''
 
 # Set a clean path on init
 set -e fish_user_paths
-set -g fish_user_paths /usr/local/opt/php/bin /usr/local/opt/coreutils/libexec/gnubin /usr/local/bin /usr/local/sbin /usr/bin /bin
+set -Ux fish_user_paths $HOME/.local/bin /usr/local/opt/php/bin /usr/local/opt/coreutils/libexec/gnubin /usr/local/bin /usr/local/sbin /usr/bin /bin
 
-# if test -e ~/.ghcup/.env
-#   bass source ~/.ghcup/.env
-# end
+#set -x LC_ALL en_US.UTF-8
+set -x LANG en_US.UTF-8
+set -x LANGUAGE en_US.UTF-8
 
-if test -e ~/.cargo/env
-    bass source ~/.cargo/env
-end
+set -x ALACRITTY_CS $HOME/.alacritty-colorschemes
 
-# Aliases
-#!/usr/bin/env bash
+alias sshc="code ~/.ssh/config"
 
 alias ga="git add"
 alias gst="git status"
@@ -46,35 +20,26 @@ alias gr="git reset --hard"
 alias gph="git push origin"
 alias gpl="git pull origin"
 
+alias vu="vagrant up"
+alias vh="vagrant halt"
+alias vr="vagrant reload --provision"
+alias vs="vagrant ssh"
+
 # PHP Aliases
-function composer74
-    "(which php) (which composer)"
-end
 if test (uname) = "Darwin"
-    function php73
+    function php74
         /usr/local/opt/php@7.3/bin/php $argv
-    end
-    function php72
-        /usr/local/opt/php@7.2/bin/php $argv
-    end
-    function php71
-        /usr/local/opt/php@7.1/bin/php $argv
-    end
-    function php70
-        /usr/local/opt/php@7.3/bin/php $argv
-    end
-    function composer73
-        php73 (which composer) $argv
-    end
-    function composer72
-        php72 (which composer) $argv
-    end
-    function composer71
-        php71 (which composer) $argv
-    end
-    function composer70
-        php70 (which composer) $argv
     end
 else
     # $argv
+end
+function homestead
+    cd ~/Union/unionco-homestead && vagrant $argv
+end
+function ph
+    cd ~/Code/personal-homestead && vagrant $argv
+end
+
+if command -v bass > /dev/null
+	bass source ~/.cargo/env > /dev/null
 end
